48733da31f25088599572c94e33ed38726ad15f8
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2018-03-22 11:32:35 -0300

    First release.

---
+++ examples/index.js
@@ -0,0 +1,42 @@
+const PardAgent = require('../')
+const uuid = require("machine-uuid-sync")();
+
+
+const agent = new PardAgent({
+  nombre: 'pardsl agent',
+  uuid: process.env.UUID || uuid,
+  interval: 1000,
+  mqtt: {
+    host: 'mqtt://10.0.0.7'
+  }
+})
+
+// agent.addMetric('rss', function getRss () {
+//   return process.memoryUsage().rss
+// })
+
+// agent.addMetric('promiseMetric', function getRandomPromise () {
+//   return Promise.resolve(Math.random())
+// })
+
+agent.addMetric('callbackMetric', function getRandomCallback (callback) {
+  setTimeout(() => {
+    callback(null, Math.random())
+  }, 1000)
+})
+
+agent.connect()
+
+// This agent only
+agent.on('connected', handler)
+agent.on('disconnected', handler)
+agent.on('message', handler)
+
+// Other Agents
+agent.on('agent/connected', handler)
+agent.on('agent/disconnected', handler)
+agent.on('agent/message', handler)
+
+function handler (payload) {
+  console.log('payload',payload)
+}
